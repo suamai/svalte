1c02ac644ad29cf2d3f3355337c3a53d44bc761e
Sua <arstrini@gmail.com>
2022-02-12 19:52:23 -0300

    feat: add basic layout and side navigation

---
+++ src/components/navigation/SideNav.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  $: pathname = $page.url.pathname;
+
+  const items = [
+    { title: 'Home', path: '/' },
+    { title: 'About', path: '/about' },
+    { title: 'Contact', path: '/contact' },
+  ];
+</script>
+
+<nav>
+  <h1>Svalte</h1>
+  <ul>
+    {#each items as { path, title }}
+      <li><a class:selected={path === pathname} href={path}>{title}</a></li>
+    {/each}
+  </ul>
+</nav>
+
+<style lang="scss">
+  nav {
+    flex-shrink: 0;
+    width: clamp(160px, 20%, 300px);
+    height: 100%;
+    border-right: 1px solid #ccc;
+    padding: 32px;
+    box-sizing: border-box;
+
+    h1 {
+      font-size: 24px;
+      margin: 0;
+      margin-bottom: 24px;
+    }
+
+    ul {
+      list-style: none;
+      padding: 0;
+      margin: 0;
+    }
+
+    li {
+      margin-bottom: 12px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    a {
+      border-radius: 8px;
+      padding: 8px;
+      color: $text-color;
+      font-size: 16px;
+      &.selected {
+        background-color: $highlight-color;
+        color: #ffffff;
+        font-weight: 600;
+      }
+      text-decoration: none;
+    }
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,21 @@
+<script>
+  import SideNav from '@/components/navigation/SideNav.svelte';
+</script>
+
+<div class="main-layout">
+  <SideNav />
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style lang="scss">
+  .main-layout {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    main {
+      padding: 32px;
+    }
+  }
+</style>
